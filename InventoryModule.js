d3a1c069c5a2aae18c33e7a4a9afa50111278937
samuelherrerak-commits <samuelherrera.k@gmail.com>
2026-07-10 15:51:48 -0400

    Add files via upload

--- InventoryModule.js
+++ InventoryModule.js
@@ -233,7 +233,18 @@ const InventoryModule = ({ onBack, initialTab = 'stock' }) => {
                     instance = new Html5Qrcode('barcode-reader-reception');
                     await instance.start(
                         { facingMode: 'environment' },
-                        { fps: 10, qrbox: { width: 300, height: 100 } },
+                        {
+                            fps: 10,
+                            formatsToSupport: [
+                                Html5QrcodeSupportedFormats.EAN_13,
+                                Html5QrcodeSupportedFormats.EAN_8,
+                                Html5QrcodeSupportedFormats.UPC_A,
+                                Html5QrcodeSupportedFormats.UPC_E,
+                                Html5QrcodeSupportedFormats.CODE_128,
+                                Html5QrcodeSupportedFormats.CODE_39,
+                                Html5QrcodeSupportedFormats.ITF,
+                            ],
+                        },
                         (code) => {
                             setItem(prev => ({ ...prev, barcode: code }));
                             const found = inventory.find(p =>
